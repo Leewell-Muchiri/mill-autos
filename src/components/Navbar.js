197ee66a10b5dda4afdf34df195b0551a85f2ad2
ianK-code <kian99564@gmail.com>
2023-03-28 14:02:51 +0300

    Added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="top-padding">
+        <logo>MILL AUTOS</logo>
+        {/* Login && signup link */}
+        <div>
+          <Link className="right-padding">About</Link>
+          <Link>Contacts</Link>
+        </div>
+        {/* Login && signup link */}
+        <div>
+          <Link className="right-padding">Login</Link>
+          <Link>Signup</Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
