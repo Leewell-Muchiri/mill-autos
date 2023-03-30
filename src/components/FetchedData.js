ec0a67d1980739a7c49dc1a8f4853c2f75eefb4f
ianK-code <kian99564@gmail.com>
2023-03-30 23:39:30 +0300

    It looks good

--- src/components/FetchedData.js
+++ src/components/FetchedData.js
@@ -1,7 +1,28 @@
 function FetchedData({ data }) {
-  return(
-    <div>
-        <h5>{data.model}</h5>
+  return (
+    <div className="fetchedData-data">
+      <div className="fetchedData-image">
+        <h1>Fetched Data</h1>
+      </div>
+      <h5>Brand: {data.brand}</h5>
+      <h5>Model: {data.model}</h5>
+      <h5>Year: {data.year}</h5>
+      <h5>Color: {data.color}</h5>
+      <h5>Price: {data.price}</h5>
+
+      <button
+        className="buy-button"
+        style={{
+          backgroundColor: data.color,
+          color: "white",
+          border: "none",
+          padding: "10px",
+          margin: "10px",
+          transition: "transform 0.5s",
+        }}
+      >
+        Buy
+      </button>
     </div>
   );
 }
