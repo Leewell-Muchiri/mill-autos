29adcf31f7d1773bea2816cd0ead33a0c6ffa803
ianK-code <kian99564@gmail.com>
2023-03-30 22:55:12 +0300

    Updated changes from linex

---
+++ src/components/Contact.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import "./Contact.css";
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Your code to handle form submission goes here
+  };
+  return (
+    <div>
+      <Navbar />
+      <div className="contact-form-container">
+        <h2>Contact Us</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Contact;
